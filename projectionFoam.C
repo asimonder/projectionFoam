f2b47f41540ca3032dd4b1e6b368a099cb63b570
Asim Onder Fugaku <asim.onder@gmail.com>
2023-02-18 22:14:01 +0900

    Updated projectionFoam.C

--- projectionFoam.C
+++ projectionFoam.C
@@ -130,7 +130,7 @@ int main(int argc, char *argv[])
 	      
 	      pEqn.setReference(pRefCell, pRefValue);
     
-	      pEqn.solve(mesh.solver(p.select(piso.finalInnerIter())));
+	      pEqn.solve(mesh.solver(p.select(piso.finalNonOrthogonalIter())));
 	      
 	      if (piso.finalNonOrthogonalIter())
 		{
